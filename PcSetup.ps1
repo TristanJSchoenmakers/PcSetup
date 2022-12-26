1d82e3be7c4c0220337a78fb7ca4725e53f9918f
Tristan J. Schoenmakers <Tristansjh@gmail.com>
2022-12-26 17:46:53 +0100

    update

--- PcSetup.ps1
+++ PcSetup.ps1
@@ -1,19 +1,17 @@
-# Script for setting up dev PC
+# Requires -RunAsAdministrator
 
-echo "---------------------------------------" 
-echo "      1 - Install applications        |" 
-echo "---------------------------------------" 
-
+Write-Host "   Installing Applications" -BackgroundColor Magenta 
 # Install all packages in winget_packages.json
 winget import $PSScriptRoot\winget_packages.json --accept-package-agreements --accept-source-agreements
 
 
-echo ""
-echo "---------------------------------------" 
-echo "|    2 - Setup Docker Containers      |" 
-echo "---------------------------------------"
+Write-Host "   Setting up docker containers" -BackgroundColor Magenta 
 
 # Stop all docker
-docker stop $(docker ps -a -q)
-docker rm $(docker ps -a -q)
-docker compose up --detach
+#docker stop $(docker ps -a -q)
+#docker rm $(docker ps -a -q)
+#docker compose up --detach
+
+Write-Host "   Configuring Applications" -BackgroundColor Magenta 
+
+"Invoke-Expression (&starship init powershell)" | Out-File ( New-Item -Path $profile -Force )
